fa2d59cf5a96f413fdbe83dc9fdc3a836a49d1e1
Nicolas <lf.nicolas45@gmail.com>
2017-03-23 16:16:28 +0100

    debut de la victoire

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wilder12
+ * Date: 23/03/17
+ * Time: 15:12
+ */
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<form action="resultat.php" method="post">
+    <label for="mot">Mot recherché</label>
+    <input type="text" name="mot" />
+    <input type="submit" name="recherche" value="Rechercher" />
+</form>
+
+</body>
+</html>

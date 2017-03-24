97ca6898abe90751545d69e808158d08bf1014c9
Julien Martin <julienmartin.opto@gmail.com>
2017-03-24 07:42:16 +0100

    css fini

--- detail.php
+++ detail.php
@@ -12,10 +12,13 @@
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="http://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
     <link rel="stylesheet" type="text/css" href="css/stylesheet.css">
+    <link href="https://fonts.googleapis.com/css?family=Anton|Passion+One|Permanent+Marker|Sigmar+One" rel="stylesheet">
 
 </head>
 
 <body>
+<a href="index.php" class="logo">EAT N RUN</a>
+
 
 <?php
 
@@ -62,21 +65,49 @@ if(isset($_GET['id']) && !empty($_GET['id'])) {
 
     if(!empty($json->products)) {
         foreach($json->products as $msg) {
-            echo $msg->product_name;
-            echo "<br/>";
-            echo "<img src=" . $msg->image_small_url . ">";
-            echo "<br/>";
-            echo $msg->brands;
-            echo "<br/>";
+            echo '<h2>'. $msg->product_name .'</h2>
+            <div class="container">
+            <div class="row">
+            <div class="col-xs-5 cadre">';
+
+            echo '
+            <img src="' . $msg->image_small_url . '">';
+
+            echo '
+<p>'. $msg->brands .'</p>';
+
             $quantity = $msg->quantity;
-            echo $msg->quantity;
-            echo "<br/>";
-            echo $msg->nutrition_grade_fr;
-            echo "<br/>";
+            echo '
+<p>'. $msg->quantity .'</p>';
+
+            switch ($msg->nutrition_grade_fr) {
+                case 'a';
+            echo '
+<img src="img/nutriscore-a.svg" class="nutri">';
+            break;
+                case 'b';
+                    echo '
+<img src="img/nutriscore-b.svg" class="nutri">';
+                    break;
+                case 'c';
+                    echo '
+<img src="img/nutriscore-c.svg" class="nutri">';
+                    break;
+                case 'd';
+                    echo '
+<img src="img/nutriscore-d.svg" class="nutri">';
+                    break;
+                case 'e';
+                    echo '
+<img src="img/nutriscore-e.svg" class="nutri">';
+                    break;
+            }
             $energy = $msg->nutriments->energy_value;
-            echo round($msg->nutriments->energy_value / 4.1868) . " kcal pour 100g";
-            echo "<br/>";
-            echo round(($msg->nutriments->energy_value * $msg->quantity / 100) / 4.1868) . " kcal";
+            echo '
+<p>'. round($msg->nutriments->energy_value / 4.1868) . ' kcal pour 100g</p>';
+
+            echo '<p>'. round(($msg->nutriments->energy_value * $msg->quantity / 100) / 4.1868) . ' kcal</p>
+            </div>';
         }
     }else {
         echo "Rien n'a été trouvé.";
@@ -87,13 +118,19 @@ if(isset($_GET['id']) && !empty($_GET['id'])) {
 
 ?>
 
-    <form action="" method="POST">
-        <div class="ui-widget">
-            <label for="search-input">Votre sport :</label>
-            <input type="text" name="sport" id="search-input" class="form-control"/>
-        </div>
-        <input type="submit" class="btn btn-default" name="search" value="Selectionner" />
-    </form>
+
+    <div class="col-xs-5 col-xs-offset-2 cadre">
+        <form action="" method="POST">
+            <div class="ui-widget choixsport">
+                <br />
+                <label for="search-input">Votre sport :</label>
+                <br />
+                <input type="text" name="sport" id="search-input" class="form-control"/>
+            </div>
+            <br />
+            <input type="submit" class="btn btn-default" name="search" value="Selectionner" />
+        </form>
+
 
 <?php
 if (isset($_POST['sport'])) {
@@ -104,13 +141,16 @@ if (isset($_POST['sport'])) {
     $req->execute();
 
     $res = $req->fetch();
-    echo '<br /><p>Vous avez choisi : '. $sport .' qui vous fait perdre : '. $res['kcalh'] .' kcal/h !</p><br />';
+    echo '<br /><br /><br /><br /><p>Vous avez choisi : '. $sport .' qui vous fait perdre : '. $res['kcalh'] .' kcal/h !</p><br />';
     $energyKcal = round(($energy/4.1868)*($quantity/100));
     $sportHours = floor($energyKcal/$res['kcalh']);
     $sportMinutes = round(($energyKcal/$res['kcalh']-floor($energyKcal/$res['kcalh']))*60);
-    echo '<p>Il faut donc que vous fassiez '. $sportHours .' heures et '. $sportMinutes .' minutes de sport !!<br />Il y a encore du taf !</p>';
+    echo '<p>Il faut donc que vous fassiez<br/>'. $sportHours .' heures et '. $sportMinutes .' minutes de sport !!<br />Il y a encore du taf !</p>';
 }
 ?>
+</div>
+</div>
+</div>
 </body>
 
 <script src="http://code.jquery.com/jquery-1.10.2.js"></script>

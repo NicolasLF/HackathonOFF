992b8cdd39c1498353e815e159ebc92542d773ed
Julien Martin <julienmartin.opto@gmail.com>
2017-03-24 08:15:03 +0100

    résolution de bugs

--- membre/index.php
+++ membre/index.php
@@ -54,7 +54,7 @@ if (isset($_POST['sp'])) {
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
           integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-    <title>Document</title>
+    <title>Eat N Run</title>
     <style>
         #gauge2 > svg > text > tspan {
             display: none !important;
